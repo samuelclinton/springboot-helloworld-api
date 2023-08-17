515062ff02f38756bdd3e9e10a03e00f9900aefc
samuelclinton <contato@samuelclinton.com>
2023-08-17 17:43:33 -0300

    Project creation

---
+++ src/main/java/com/samuelclinton/helloworldapi/HelloworldApiApplication.java
@@ -0,0 +1,13 @@
+package com.samuelclinton.helloworldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApiApplication.class, args);
+	}
+
+}
